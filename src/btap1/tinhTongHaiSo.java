2cd115eece7c80eb15457e2649c0f839bce546ea
DungCR7 <buivandung279@gmail.com>
2021-11-26 15:20:07 +0700

    Init project

---
+++ src/btap1/tinhTongHaiSo.java
@@ -0,0 +1,25 @@
+package btap1;
+
+import java.util.Scanner;
+
+public class tinhTongHaiSo {
+    private int instanceVarialbe;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a,b;
+        System.out.println("Nhap a : ");
+        a = scanner.nextInt();
+        System.out.println("Nhap b : ");
+        b = scanner.nextInt();
+        int tong = a + b ;
+        System.out.println("Tong la : "+tong);
+        int hieu = a - b ;
+        System.out.println("Hieu la : "+hieu);
+        int tich = a*b;
+        System.out.println("Tich la : "+tich);
+        int thuong = a / b;
+        System.out.println("Thuong la : "+thuong);
+        int mod = a / b;
+        System.out.println("Mod la : "+mod);
+    }
+}
